b11ef9cb40e6ba3467337e6ae8b14b0240a0522b
Daniel Carvalho <59699633+badcons2002@users.noreply.github.com>
2022-03-07 21:19:33 -0300

    Gerador d apostas em ordem não repetidos de 1 a 60

---
+++ Mega-sena-apostas-gerador.py
@@ -0,0 +1,18 @@
+from random import *
+
+def geranumeros():
+    aposta = []
+    i = 1
+    while i <= 6:
+        numero = randrange(1, 60)
+        if not numero in aposta:
+            aposta.append(numero)
+            i+= 1
+    aposta.sort()
+    return aposta
+    
+i= 1
+while i<= 10:
+    aposta = tuple(geranumeros())
+    print(f"Aposta {i}:_{aposta}")
+    i+= 1
